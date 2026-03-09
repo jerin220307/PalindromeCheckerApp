a58bd6fb6e16fbcc5b59806c2ce21d59a913c7a9
jerin220307 <jerin220307@gmail.com>
2026-03-09 13:41:33 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,25 +1,5 @@
 import java.util.Scanner;
 
-public class PalindromeCheckerApp {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter text: ");
-        String input = sc.nextLine();
-
-        String reversed = "";
-
-        for(int i=input.length()-1;i>=0;i--){
-            reversed += input.charAt(i);
-        }
-
-        if(input.equals(reversed))
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not Palindrome");
-
-        sc.close();
-    }
-}
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter text: ");
+String input = sc.nextLine();
