3d83abfc86623c3b5c95bcbc2c0235f102fef025
Jerin <jr2218@srmist.edu.in>
2026-03-09 11:10:42 +0530

    UC2: HardCodePalindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,17 @@
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Application");
+
+        String word = "madam";
+        String reverse = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverse = reverse + word.charAt(i);
+        }
+
+        if (word.equals(reverse))
+            System.out.println(word + " is a Palindrome");
+        else
+            System.out.println(word + " is not a Palindrome");
     }
 }
-
