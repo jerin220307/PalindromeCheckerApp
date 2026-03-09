ea09e61eb7985c7aea36db5dbbc9a3f3c226efd5
Jerin <jr2218@srmist.edu.in>
2026-03-09 12:42:36 +0530

    UC9: Palindrome check using method

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,1 +1,23 @@
-input = input.replaceAll("[^a-zA-Z0-9]", "");
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+    public static boolean isPalindrome(String word) {
+        String reversed = new StringBuilder(word).reverse().toString();
+        return word.equalsIgnoreCase(reversed);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String input = sc.nextLine();
+
+        if(isPalindrome(input))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+
+        sc.close();
+    }
+}
